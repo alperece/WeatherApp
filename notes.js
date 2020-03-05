b1b9268ec598af813f8eab5ee9befcdc2e3c7ab8
alperece <49275650+alperece@users.noreply.github.com>
2020-03-05 14:53:02 +0100

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,24 @@
+// 1- Weather status from API like "data.weather[0].main;" and store as a variable
+
+// 2- Write a function line "function weatherSet(status){}"
+
+// 3- Inside the function "if ( status == "Clear"){}" do select the videoID (id="wheater-video") and use ".html("<video autoplay muted loop " + 'id="myVideo"><source  src="videos/clearNight.mp4" type="video/mp4"></video>');"
+
+// 4- weather conditions are ( Clear, Rain, Clouds,Snow,Mist,Thunderstorm). Do not forget the change video name in above code src!
+
+/*  5- Add this inside the html 
+( <div id="wheater-video"><video autoplay muted loop id="myVideo"><source src="" type="video/mp4"></video></div> ) */
+
+// 6- You need the call the funcition weatherSet inside the displayWeather
+
+
+
+/* 
+
+Give the delivery guys a GPS or take geolocations from their phones
+Show the root path on google map
+How many km did they do every day?
+How much benzin(gasoline) did he spend?
+Show their location instantly on map and send information to customer
+
+*/
